932f2a3f2471fbdf0bd11065840209627b9130cf
Jakob Fastenbauer <jakob.fastenbauer@kontron.com>
2022-03-20 20:15:47 +0100

    code smell

--- src/app/presentation/country-detail/country-detail.component.ts
+++ src/app/presentation/country-detail/country-detail.component.ts
@@ -1,6 +1,5 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Country} from "../../models/country.model";
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-country-detail',
